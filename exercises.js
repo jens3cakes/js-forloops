caf6b3786599dad68ff42c29205f3b4b52fe7d95
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2018-07-12 21:56:52 -1000

    up to 11

--- exercises.js
+++ exercises.js
@@ -76,30 +76,41 @@ function leaders(person) {
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
-var stringOfNumbers = ("");
+var stringOfNumbers = "";
  
- function conNum() {
    for (var i = 10; i < 21; i++) {
-     console.log(stringOfNumbers + 1)
+     stringOfNumbers += i;
    }
- }
- conNum();
+ console.log(stringOfNumbers);
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
 
 Write a for-loop that will push even numbers to the `evenNumArr` array. We want to push 50 even even numbers starting from 0.
 Console.log your results.
 */
+var evenNumArr = [];
 
-
-
+for (var i = 0; i < 101; i++) {
+  if(i % 2 === 0) {
+  evenNumArr.push(i);
+}
+}
+console.log(evenNumArr);
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
 
 Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want to add 50 odd numbers starting from 1.
 Console.log your results.
 */
-  
+var oddSum = 0;
+
+for (var i = 1; i<101; i++) {
+  if (i % 2 !== 0) {
+    oddSum+=i
+  }
+}
+  console.log(oddSum);
+
 
 /* 7) Oops There It is
 Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
@@ -107,6 +118,16 @@ Note that every odd index value in `oopsArray` is currently `undefined`. Using a
 
 [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]
 */
+
+var oopsArray = ["turn", , "down", , "for", , "what"];
+for (var i = 0; i<oopsArray.length; i++){
+  if (i %2 !== 0){
+    oopsArray[i] = "nope"
+  }else{
+   // console.log(oopsArray[i])
+  }
+};
+console.log(oopsArray);
 
 
 /* 8) Is It There Oops
@@ -120,6 +141,7 @@ down
 nope
 turn
 */
+console.log(oopsArray.reverse());
 
 
 /* 9) Siesta Time
@@ -131,7 +153,17 @@ Next, write a function named `nap`. This function takes in a single parameter: `
 
 Inside of this function write a for-loop that will iterate through the `napSchedule` array and console.log the message: `ZzZzZzZz` if the schedule is `true`, otherwise the it will console.log the message: `Gotta get coding!` if the schedule is `false`.
 */
-
+var napSchedule = [false, false, true, false, true, true];
+function nap(schedule) {
+  for (var i = 0; i<schedule.length; i++) {
+    if (schedule[i] == false) {
+      console.log("Gotta get coding!")
+      } else {
+        console.log("ZzZzZzZz");
+      }
+    }
+  };
+console.log(nap(napSchedule));
 
 
 /* 10) Copy Pasta
@@ -154,7 +186,20 @@ Inside of this function write a for-loop that will iterate through the contents 
 Declare a variable named `topQuote` and assign it to a String value of your favorite one line quote.
 Write a function that will iterate through the string value and return the longest word in that quote. Console.log your result.
 */
+var topQuote = "Do or do not, there is no try";
+function longestWord(str) {
+var strToArray = str.split(" ");
+var currentWord = "";
+for (var i = 0; i< strToArray.length; i++) {
+  if (strToArray[i].length > currentWord.length) {
+    currentWord = strToArray[i]
+  }
+  }
+  return currentWord;
+}
 
+
+console.log(longestWord(topQuote));
 
 
 /* 12) Puppet Master
@@ -185,6 +230,14 @@ Note, please use two for loops to complete this problem
 */
 var guide = [["Honolulu", "Waikiki"], ["Tokyo", "Tsukiji Fish Market"], ["Cairo", "Pyramids"],["Rome", "Colosseum"]];
 
+function cityLand(str) {
+for (var i = 0; i <str.length; i++){
+  for (var j = 0; j<str[i].length;j++){
+  console.log(str[i][j]);
+}
+}
+}
+cityLand(guide);
 
 
 /* 15) Back to School
